3722bfca5a9a36d27fa82b459aa376358a34fb19
Phumipat Peamlue <besto@Phumipats-MacBook-Pro.local>
2022-11-21 17:45:01 +0700

    add header footer and fix deleteBtn

--- public/addSubject.js
+++ public/addSubject.js
@@ -8,24 +8,24 @@ export const addSubject = async (db, subjectCode) => {
   const code = document.createElement("td");
   const name = document.createElement("td");
   const credit = document.createElement("td");
-  const div = document.createElement("div");
-  const deleteBtn = document.createElement("p");
+  const deleteBtn = document.createElement("td");
+  const deleteText = document.createElement("p");
 
   code.innerHTML = subject.code;
   name.innerHTML = subject.name;
   credit.innerHTML = subject.credit;
-  deleteBtn.innerHTML = "delete";
-  deleteBtn.addEventListener("click", (e) => {
+  deleteText.innerHTML = "delete";
+  deleteText.addEventListener("click", (e) => {
     deleteHandler(tr, subject.type, subject.credit);
   })
   
-  div.id = "deleteBtn";
-  div.appendChild(deleteBtn);
+  deleteBtn.id = "deleteBtn";
+  deleteBtn.appendChild(deleteText);
 
   tr.appendChild(code);
   tr.appendChild(name);
   tr.appendChild(credit);
-  tr.appendChild(div);
+  tr.appendChild(deleteBtn);
 
   subjectTable.appendChild(tr);
 }
